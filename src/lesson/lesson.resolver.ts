440572cdf81a3544ac6c4ae2895ed56ee8c21fa3
Magdalena Krezheska <Magdalena.Krezheska@netcetera.com>
2020-09-01 10:21:24 +0200

    Input validation, getLessons

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,6 +1,7 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
@@ -9,21 +10,30 @@ export class LessonResolver {
   ) {}
 
   @Query(returns => LessonType)
-  lesson() {
-    return {
-      id: 'asdjo12j31a',
-      name: 'Physics Class',
-      startDate: (new Date()).toISOString(),
-      endDate: (new Date()).toISOString()
-    };
+  lesson(
+    @Args('id') id:string,
+  ) {
+    return this.lessonService.getLesson(id);
   }
+
+  @Query(returns => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
+  }
+
+  // @Mutation(returns => LessonType)
+  // createLesson(
+  //   @Args('name') name: string,
+  //   @Args('startDate') startDate: string,
+  //   @Args('endDate') endDate: string
+  // ) {
+  //   return this.lessonService.createLesson(name, startDate, endDate);
+  // }
 
   @Mutation(returns => LessonType)
   createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string
+    @Args('createLessonInput') createlessonInput: CreateLessonInput,
   ) {
-    return this.lessonService.createLesson(name, startDate, endDate);
+    return this.lessonService.createLesson(createlessonInput);
   }
 }
